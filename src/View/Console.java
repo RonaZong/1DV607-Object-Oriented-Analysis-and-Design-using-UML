3a536a3fc0e59f4f6fc96b30f6b038a3c5bfc6ad
zhuxiaohe <xiaosheshe97@gmail.com>
2020-09-23 12:47:01 +0200

    Update Console.java

--- src/View/Console.java
+++ src/View/Console.java
@@ -7,8 +7,16 @@ public class Console {
 
     public void start(){
 
-        System.out.println("Hello china" +
-                "try again");
+        System.out.println("Welcome to Boat Club");
+        System.out.println("Press 1 to create a new member");
+        System.out.println("Press 2 to show lists of all members");
+        System.out.println("Press 3 to change a member’s information");
+
+
+
+    }
+
+    public void createMember(){
 
     }
 }
