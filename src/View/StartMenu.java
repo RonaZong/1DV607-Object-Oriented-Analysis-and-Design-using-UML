670f93dd5251609480183420514c829ab74ca2c9
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-23 13:06:56 +0200

    Create StartMenu.java

---
+++ src/View/StartMenu.java
@@ -0,0 +1,26 @@
+package View;
+
+public class StartMenu {
+
+    public StartMenu() {
+    }
+
+    public void start(){
+
+        System.out.println("Welcome to Boat Club\n" +
+                "Press 1 to create a new member\n" +
+                "Press 2 to show lists of all members\n" +
+                //who can have access to a specific member's information?
+                "press 3 to check member's information ");
+                // do we need to have an admin so can have access to member's information?
+               
+
+
+
+
+    }
+
+    public void createMember(){
+
+    }
+}
