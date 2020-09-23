1f57ce54f3d4392e19685aa86c7d043a41eed8a5
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-24 01:38:31 +0200

    Update Member.java

--- src/Model/Member.java
+++ src/Model/Member.java
@@ -1,24 +1,37 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Member {
     private String name;
     private String personalNumber;
-    private String memberID;
-    private Boat boats;
-    private Scanner scanner = new Scanner(System.in);
+    private int memberID =1;
+    //we need the know how many boats each member has
+    //if member should have a boat at least so he or she can register we can initialize arraylist in method
+    //otherwise we have to initialize arraylist here or we get null value
+    private ArrayList<Boat> boats;
+    
+    // I think we don't need scanner here
+   // private Scanner scanner = new Scanner(System.in);
 
     public Member(String name, String personalNumber) {
         this.name = name;
         this.personalNumber = personalNumber;
+        creatUniqueID();
     }
 
-    public Member(String name, String personalNumber, String memberID) {
+    public Member(){
+
+    }
+
+   // sorry to comment this out I wrote a comment below and I will explain tomorrow
+   /* public Member(String name, String personalNumber, int memberID) {
         this.name = name;
         this.personalNumber = personalNumber;
         this.memberID = memberID;
-    }
+    }*/
+
 
     public String getName() {
         return name;
@@ -36,11 +49,25 @@ public class Member {
         this.personalNumber = personalNumber;
     }
 
-    public String getMemberID() {
+    public int getMemberID() {
         return memberID;
     }
 
-    public void setMemberID(String memberID) {
-        this.memberID = memberID;
+    //public void setMemberID(int memberID) {
+      //  this.memberID = memberID;
+    //}
+    // I m not sure if it works or not but the member ID should be created by system and
+    // we cant set it to whatever we want so I think we dont need setter
+    public int creatUniqueID(){
+        return memberID++;
+    }
+
+    public void addBoat(Boat boat){
+        boats = new ArrayList<>();
+        boats.add(boat);
+    }
+
+    public ArrayList<Boat> boatsOwnedByMember(){
+        return this.boats;
     }
 }
