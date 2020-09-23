ee192f79f5f8397cfa265a4a6ac1407427384f03
Xingrong Zong <xz222bb@student.lnu.se>
2020-09-23 11:19:49 +0200

    Update Member.java

--- src/Model/Member.java
+++ src/Model/Member.java
@@ -1,4 +1,31 @@
 package Model;
 
 public class Member {
+    private String name;
+    private String personalNumber;
+    private String memberID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(String personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
 }
