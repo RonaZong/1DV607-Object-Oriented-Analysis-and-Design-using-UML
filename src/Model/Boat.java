bada5a12073151eff57e5d2e498caf85cb9c5b99
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-24 01:40:13 +0200

    Update Boat.java

--- src/Model/Boat.java
+++ src/Model/Boat.java
@@ -1,19 +1,25 @@
 package Model;
 
+
 public class Boat {
-    private String type;
+    public enum BoatType {
+        SAILBOAT, MOTORSAILOR, KAYAK_OR_CANOE, OTHER
+    }
+
+    private BoatType type;
     private int length;
 
-    public Boat(String type, int length) {
+    //since we have only this 4 fixed type better to have enum in my idea
+    public Boat(BoatType type, int length) {
         this.type = type;
         this.length = length;
     }
 
-    public String getType() {
+    public BoatType getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(BoatType type) {
         this.type = type;
     }
 
