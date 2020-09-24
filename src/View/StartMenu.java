19627be11712aa4737310af7e453b43725b7f377
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-24 19:32:38 +0200

    Update StartMenu.java

--- src/View/StartMenu.java
+++ src/View/StartMenu.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class StartMenu {
     private Scanner sc;
     private int userInput;
-    private boolean alreadyMember;
+    private boolean alreadyMember=false;
 
     public StartMenu() {
     }
@@ -24,24 +24,28 @@ public class StartMenu {
     }
 
     private boolean IWantToQuit(){
-        return userInput==4;
+        return userInput==3;
     }
 
     public void welcomeMessage(BoatClub boatClub){
 
         while (!IWantToQuit()) {
-            System.out.println("Welcome to Boat Club\n" +
-                    "-----------------------\n" +
-                    "If you are not a member press 1 to register as a new member\n" +
-                    "Otherwise press 2 to show members' menu\n" +
-                    //who can have access to a specific member's information?
-                   // "press 3 to check member's information \n" +
-                    "press 4 to quit \n");
-            // do we need to have an admin so can have access to member's information?
+            if(!alreadyMember) {
+                System.out.println("Welcome to Boat Club\n" +
+                        "-----------------------\n" +
+                        "If you are not a member press 1 to register as a new member\n" +
+                        "Otherwise press 2 to show members' menu\n" +
+                        //who can have access to a specific member's information?
+                        // "press 3 to check member's information \n" +
+                        "press 3 to quit \n");
+                // do we need to have an admin so can have access to member's information?
 
-            userInput = userIntInput();
 
-            actUponUserInput(userInput , boatClub);
+                userInput = userIntInput();
+
+                actUponUserInputInMainMenu(userInput, boatClub);
+            }
+            showMemberMenu();
            /* int choice = sc.nextInt();
             switch (choice) {
                 case 1:
@@ -57,21 +61,57 @@ public class StartMenu {
 
     public void showMemberMenu(){
         System.out.println("----Members' menu----");
-        System.out.println("For see a compact list of members press 5 ");
-        System.out.println("For see a full detailed list of members press 6");
-        System.out.println("For see a full detailed list of members press 6");
-        System.out.println("For see a full detailed list of members press 6");
+        System.out.println("For see a compact list of members press 4 ");
+        System.out.println("For see a full detailed list of members press 5");
+        System.out.println("For delete a member press 6");
+        System.out.println("For update the information of a member press 7");
+        System.out.println("For see the information of a specific member press 8");
+        System.out.println("For register a new boat press 9");
+        System.out.println("For update a boat information press 10");
+        System.out.println("For delete a boat press 11");
+        System.out.println("For quit press 3");
+        int userInput = userIntInput();
     }
 
-    public void actUponUserInput(int userInput , BoatClub boatClub) {
+    public void actUponUserInputInMainMenu(int userInput , BoatClub boatClub) {
         switch (userInput) {
             case 1:
                 createMemberMenu(boatClub);
                 break;
             case 2:
-                showList(boatClub);
+                showMemberMenu();
                 break;
         }
+    }
+
+    public void actUponUserInputInMemberMenu(int userInput , BoatClub boatClub , Member member){
+        switch(userInput){
+            case 4:
+                showCompactList(boatClub);
+                break;
+            case 5:
+                showVerboseList(boatClub);
+                break;
+            case 6:
+                boatClub.deleteMember(member);
+                break;
+            case 7:
+                showUpdateMemberMenu(boatClub , member);
+                break;
+
+        }
+    }
+
+    private void showUpdateMemberMenu(BoatClub boatClub , Member member) {
+        System.out.println("What do you want to update?");
+        System.out.println("If you want to update name enter your new name otherwise press enter");
+        String name = userStringInput();
+        //do we need to check for validate personal number??
+        System.out.println("if you want to update your personal number enter your new personal" +
+                "number otherwise press enter");
+        String personalNumber = userStringInput();
+        boatClub.updateMemberInformation(member , name , personalNumber);
+
     }
 
     public void showList(BoatClub boatClub){
@@ -98,6 +138,7 @@ public class StartMenu {
         System.out.print("Please enter personal number: ");
         String personalNumber = userStringInput();
         boatClub.creatMember(name,personalNumber);
+        this.alreadyMember = true;
 
 
     }
