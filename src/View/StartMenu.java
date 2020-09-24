394bf34f7ed070fa71fb66891b8cbbc7d024e653
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-24 17:42:26 +0200

    Update StartMenu.java

--- src/View/StartMenu.java
+++ src/View/StartMenu.java
@@ -23,19 +23,19 @@ public class StartMenu {
         return sc.nextLine();
     }
 
-    private boolean iWantToQuit(){
+    private boolean IWantToQuit(){
         return userInput==4;
     }
 
-    public void start(BoatClub boatClub){
+    public void welcomeMessage(BoatClub boatClub){
 
-        while (!iWantToQuit()) {
+        while (!IWantToQuit()) {
             System.out.println("Welcome to Boat Club\n" +
                     "-----------------------\n" +
-                    "Press 1 to register as a new member\n" +
-                    "Press 2 to show lists of all members\n" +
+                    "If you are not a member press 1 to register as a new member\n" +
+                    "Otherwise press 2 to show members' menu\n" +
                     //who can have access to a specific member's information?
-                    "press 3 to check member's information \n" +
+                   // "press 3 to check member's information \n" +
                     "press 4 to quit \n");
             // do we need to have an admin so can have access to member's information?
 
@@ -55,10 +55,18 @@ public class StartMenu {
         }
     }
 
+    public void showMemberMenu(){
+        System.out.println("----Members' menu----");
+        System.out.println("For see a compact list of members press 5 ");
+        System.out.println("For see a full detailed list of members press 6");
+        System.out.println("For see a full detailed list of members press 6");
+        System.out.println("For see a full detailed list of members press 6");
+    }
+
     public void actUponUserInput(int userInput , BoatClub boatClub) {
         switch (userInput) {
             case 1:
-                showMembershipMenu(boatClub);
+                createMemberMenu(boatClub);
                 break;
             case 2:
                 showList(boatClub);
@@ -81,45 +89,44 @@ public class StartMenu {
     }
 
     public void createMemberMenu(BoatClub boatClub){
-        System.out.println("----- Add a Member -----");
+        System.out.println("----- Become a Member -----");
 
+        System.out.println("In order to be a member you have to enter following information : ");
         System.out.print("Please enter user name: ");
-        String ch=sc.nextLine();
-        String name = sc.nextLine();
+        //String ch=sc.nextLine();????
+        String name = userStringInput();
         System.out.print("Please enter personal number: ");
-        String personalNumber = sc.next();
+        String personalNumber = userStringInput();
         boatClub.creatMember(name,personalNumber);
 
 
     }
 
-    public void showCompactList(Model.Member member){
-        System.out.println("This member name is : " + member.getName()+
-                           "\nwith memberID of : " + member.getMemberID()+
-                            "\nwhich has " + member.boatsOwnedByMember().size() + "boats");
-    }
-
-    public void showVerboseList(Model.Member member){
-        System.out.println("This member name is : " + member.getName() +
-                           "\nwith personal number of " + member.getPersonalNumber() +
-                            "\nwith memberID of " + member.getMemberID());
-        System.out.println("This member has " + member.boatsOwnedByMember().size() + "boats");
-        if(member.boatsOwnedByMember().size()> 0 ){
-            System.out.println("this member boat information is :");
-            for(Boat boat : member.boatsOwnedByMember()){
-                System.out.println("Boat type :" + boat.getType() +
-                                   "\nBoat color : " + boat.getLength());
-            }
+    public void showCompactList(BoatClub boatClub){
+        for(Member member : boatClub.getAllMember()) {
+            System.out.println("This member name is : " + member.getName() +
+                    "\nwith memberID of : " + member.getMemberID() +
+                    "\nwhich has " + member.boatsOwnedByMember().size() + "boats" +
+                    "\n------------\n");
         }
     }
 
-    private void showMembershipMenu(BoatClub boatClub) {
-        System.out.println("In oreder to become a member u have to " +
-                "enter following information:" +
-                "\nEnter your full name : ");
-        String fullName =userStringInput();
-        System.out.println("Enter your personal number : ");
-        String personalNumber = userStringInput();
-        boatClub.creatMember(fullName , personalNumber);
+    public void showVerboseList(BoatClub boatClub){
+        for(Member member : boatClub.getAllMember()){
+        System.out.println("This member name is : " + member.getName() +
+                           "\nwith personal number of " + member.getPersonalNumber() +
+                            "\nwith memberID of " + member.getMemberID());
+        //it might give a null exception
+        System.out.println("This member has " + member.boatsOwnedByMember().size() + "boats");
+        if(member.boatsOwnedByMember().size()> 0 ) {
+            System.out.println("this member boat information is :");
+            for (Boat boat : member.boatsOwnedByMember()) {
+                System.out.println("Boat type :" + boat.getType() +
+                        "\nBoat color : " + boat.getLength());
+            }
+        }
+        }
     }
+
+
     }
