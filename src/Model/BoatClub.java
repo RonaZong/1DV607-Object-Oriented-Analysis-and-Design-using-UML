289cbb51677149953753ff1f2ec53ca9a9f1a528
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-24 17:42:21 +0200

    Update BoatClub.java

--- src/Model/BoatClub.java
+++ src/Model/BoatClub.java
@@ -3,13 +3,14 @@ package Model;
 import java.util.ArrayList;
 
 public class BoatClub {
-    private ArrayList<Member> members;
+    private ArrayList<Member> members=new ArrayList<>();
 
     public BoatClub() {
     }
 
     public void creatMember(String userName, String personalNumber){
         Member member = new Member(userName,personalNumber);
+        members.add(member);
 
     }
 
