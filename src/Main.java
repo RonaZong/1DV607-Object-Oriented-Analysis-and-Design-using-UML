bb762076d4ecc5524e981ea6efb6a3807aaf6798
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-25 20:17:20 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int input = 1;
+
         BoatClub boatClub = new BoatClub();
         Menu console = new StartMenu();
-        console.showInstruction(1,console);
+        console.showInstruction(console);
     }
 }
