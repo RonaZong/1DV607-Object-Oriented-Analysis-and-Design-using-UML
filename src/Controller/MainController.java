50cb5ae7f5f1d5aaa8232aacd6eac7489423696e
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-28 02:40:10 +0200

    Update MainController.java

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,8 +1,7 @@
 package Controller;
 
 import Model.BoatClub;
-import View.Menu;
-import View.StartMenu;
+import View.*;
 
 public class MainController {
 
@@ -19,7 +18,47 @@ public class MainController {
     public void memberAction(BoatClub boatClub){
 
         this.menu.showInstruction();
+        actUponUserInputInStartMenu();
         controller.userWantsToAddMember(boatClub);
-       // menu.showInstruction();
+
+    }
+
+    private void actUponUserInputInStartMenu(  ) {
+        StartMenu menu = new StartMenu();
+        boolean IWantToQuit = false;
+        while(IWantToQuit) {
+            StartMenu.UserChoiceInMainMenu userChoice = menu.getUserInputInMainMenu();
+            switch (userChoice) {
+                case ADD_NEW_MEMBER:
+                    showRegisterMenu();
+                    IWantToQuit = true;
+                    break;
+                case MEMBER_MENU:
+                    showMemberMenu();
+                    break;
+                case BOAT_MENU:
+                    showBoatMenu();
+                    break;
+                case QUIT:
+IWantToQuit=true;
+                    break;
+
+            }
+        }
+    }
+
+    private void showBoatMenu() {
+        Menu menu = new BoatMenu();
+        menu.showInstruction();
+    }
+
+    private void showMemberMenu() {
+        Menu menu = new MemberMenu();
+        menu.showInstruction();
+    }
+
+    private void showRegisterMenu() {
+        Menu menu = new MemberCreationMenu();
+        menu.showInstruction();
     }
 }
