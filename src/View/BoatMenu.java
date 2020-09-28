6a23ab675c665e23478ee09fbb0c8726e50cb6ed
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-28 03:01:52 +0200

    Update BoatMenu.java

--- src/View/BoatMenu.java
+++ src/View/BoatMenu.java
@@ -1,13 +1,33 @@
 package View;
 
+import Model.Boat;
+import Model.Member;
+
+import java.util.Scanner;
+
 public class BoatMenu extends Menu {
 
-    public void showMenu() {
-    }
+    private Scanner sc;
+
+
 
     @Override
     public void showInstruction() {
 
         System.out.println("enter your boat information");
+    }
+
+    private int userIntInput(){
+        sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    private void askForABoatDataToRegister(Member member, Boat boat) {
+        System.out.println("enter boat type:" +
+                "\n0 for Sailboat , 1 for Motor sailor , 2 for Kayak/Canoe, 3 for Others");
+        int typeValue = userIntInput();
+        System.out.println("Enter length of the boat");
+        int length = userIntInput();
+        member.registerNewBoat(Boat.BoatType.values()[typeValue] , length);
     }
 }
