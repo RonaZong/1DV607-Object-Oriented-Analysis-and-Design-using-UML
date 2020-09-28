943dbead8810c03d741c9fad0159a759adaf6df0
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-28 18:42:26 +0200

    Update CompactListSave.java

--- src/Model/CompactListSave.java
+++ src/Model/CompactListSave.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CompactListSave {
@@ -17,6 +18,7 @@ public class CompactListSave {
                 saver.println(index + "," + member.getName() + "," + member.getMemberID() + "," + member.numberOfBoats());
                 index++;
             }
+            saver.close();
 
         }catch(Exception e){
 
@@ -24,5 +26,36 @@ public class CompactListSave {
         return file;
     }
 
-    
+    public String loadFromCompactList(File file){
+        ArrayList<Member> members = new ArrayList<>();
+        String result = "";
+       try {
+           reader = new Scanner(file);
+           while (reader.hasNextLine()){
+               String thisLine = reader.nextLine();
+               String[] parameters = thisLine.split(",");
+               Member member = new Member(parameters[1], parameters[2]);
+               member.setNumbersOfBoatsOwnByAMember(Integer.parseInt(parameters[3]));
+
+
+               result+=thisLine;
+           }
+       }catch(Exception e){
+
+       }
+       return result;
+    }
+
+    public Iterable<Member> readyToPrintForCompactList(String result){
+        ArrayList<Member> members = new ArrayList<>();
+        String[] eachLines = result.split("[\\r\\n]+");
+        for (String lines: eachLines) {
+            String[] parameters = lines.split(",");
+            Member member = new Member(parameters[1],parameters[2]);
+            member.setNumbersOfBoatsOwnByAMember(Integer.parseInt(parameters[3]));
+            members.add(member);
+        }
+        return members;
+    }
+
 }
