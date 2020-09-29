eccbc4f142c41073d6da2f47d7dd1c9545b34b84
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-29 23:45:37 +0200

    Update MemberCreationMenu.java

--- src/View/MemberCreationMenu.java
+++ src/View/MemberCreationMenu.java
@@ -1,6 +1,7 @@
 package View;
 
 import Model.BoatClub;
+import Util.BoatType;
 
 import java.util.Scanner;
 
@@ -9,10 +10,22 @@ public class MemberCreationMenu extends Menu {
     private Scanner sc;
     private String name;
     private String personalNumber;
+    private int numberOfBoats;
+    private double[] boatLength;
+    private int typeOfBoat;
+    private BoatType[] type;
+
+
+
 
     private String userStringInput(){
         sc =new Scanner(System.in);
         return sc.nextLine();
+    }
+
+    private int userIntInput(){
+        sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 
     @Override
@@ -25,6 +38,15 @@ public class MemberCreationMenu extends Menu {
             System.out.print("Please enter personal number in 10 digits: ");
             this.personalNumber = userStringInput();
         }while (!isValid(personalNumber));
+        System.out.println("Please enter numbers of boats:");
+        this.numberOfBoats = userIntInput();
+        for(int i = 0 ; i<numberOfBoats;i++){
+            System.out.println("Please enter length of the boat:");
+            boatLength[i] = sc.nextDouble();
+            System.out.println("Please enter boat type(1 for sailboat, 2 for motor sail, " +
+                    "3 for kayak/canoe, and 4 for others");
+            type[i] = BoatType.values()[i];
+        }
     }
 
     private boolean isValid(String input){
@@ -41,5 +63,17 @@ public class MemberCreationMenu extends Menu {
 
     public void confirmationMsg(){
         System.out.println(this.name+" is added");
+    }
+
+    public int getNumberOfBoats(){
+        return numberOfBoats;
+    }
+
+    public double[] getBoatLength() {
+        return boatLength;
+    }
+
+    public BoatType[] getType() {
+        return type;
     }
 }
