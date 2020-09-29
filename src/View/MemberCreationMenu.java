36030b0cd99da362ff9fc3694e45c8aa1e553620
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-30 00:53:19 +0200

    Update MemberCreationMenu.java

--- src/View/MemberCreationMenu.java
+++ src/View/MemberCreationMenu.java
@@ -28,6 +28,11 @@ public class MemberCreationMenu extends Menu {
         return sc.nextInt();
     }
 
+    private double userDoubleInput(){
+        sc = new Scanner(System.in);
+        return sc.nextDouble();
+    }
+
     @Override
     public void showInstruction( ) {
         System.out.println("----- Add a Member -----\n" +
@@ -40,12 +45,14 @@ public class MemberCreationMenu extends Menu {
         }while (!isValid(personalNumber));
         System.out.println("Please enter numbers of boats:");
         this.numberOfBoats = userIntInput();
+        boatLength = new double[numberOfBoats];
+        type = new BoatType[numberOfBoats];
         for(int i = 0 ; i<numberOfBoats;i++){
             System.out.println("Please enter length of the boat:");
-            boatLength[i] = sc.nextDouble();
+            boatLength[i] = userDoubleInput();
             System.out.println("Please enter boat type(1 for sailboat, 2 for motor sail, " +
-                    "3 for kayak/canoe, and 4 for others");
-            type[i] = BoatType.values()[i];
+                    "3 for kayak/canoe, and 4 for others)");
+            type[i] = BoatType.values()[userIntInput()-1];
         }
     }
 
