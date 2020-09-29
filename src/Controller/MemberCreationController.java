4c0b7d9ccdeb8f52dcf6403d202e219261d9db04
Rahim Pormah <rp222hu@student.lnu.se>
2020-09-29 23:46:04 +0200

    Update MemberCreationController.java

--- src/Controller/MemberCreationController.java
+++ src/Controller/MemberCreationController.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import Model.BoatClub;
+import Model.Member;
+import Util.BoatType;
 import View.MemberCreationMenu;
 import View.Menu;
 import View.StartMenu;
@@ -15,7 +17,10 @@ public class MemberCreationController {
 
         menu=new MemberCreationMenu();
         menu.showInstruction();
-        boatClub.creatMember(menu.getName() , menu.getPersonalNumber());
+        Member member=boatClub.creatMember(menu.getName() , menu.getPersonalNumber());
+        for(int i = 0 ; i < menu.getNumberOfBoats();i++){
+            member.registerNewBoat(menu.getType()[i] , menu.getBoatLength()[i]);
+        }
         menu.confirmationMsg();
     }
 
