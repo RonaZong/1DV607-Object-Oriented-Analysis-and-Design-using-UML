e7271b71961eea7e17e541b5e061c41b95739cc9
Xingrong Zong <xz222bb@student.lnu.se>
2020-09-30 23:10:21 +0200

    Update MemberMenuController.java

--- src/Controller/MemberMenuController.java
+++ src/Controller/MemberMenuController.java
@@ -9,9 +9,10 @@ import View.StartMenu;
 public class MemberMenuController {
 
     private MemberMenu menu;
+    private Member member;
 
     public void actUponUserInputInMemberMenu(BoatClub boatClub) {
-        MemberMenu menu = new MemberMenu();
+        menu = new MemberMenu();
         boolean IWantToGoBack = false;
         while (!IWantToGoBack) {
             menu.showInstruction();
@@ -24,15 +25,15 @@ public class MemberMenuController {
                case VERBOSE_LIST:
                     menu.showVerboseList(boatClub);
                     break;
-               /*  case DELETE:
+               case DELETE:
                     menu.showDeleteMemberMenu();
                     boatClub.deleteMember(member);
                     break;
-                case UPDATE:
+               case UPDATE:
                     menu.showUpdateMenu();
                     boatClub.updateMemberInformation(member,menu.getName(),menu.getPersonalNumber());
                     break;
-                case SPECIFIC_MEMBER:*/
+               case SPECIFIC_MEMBER:
 
             }
 
