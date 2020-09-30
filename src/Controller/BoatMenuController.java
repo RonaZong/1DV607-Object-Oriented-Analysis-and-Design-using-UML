c955accb03f67a78167cf7db2fa2ae16b3b19c23
zhuxiaohe <xiaosheshe97@gmail.com>
2020-09-30 12:00:27 +0200

    Update BoatMenuController.java

--- src/Controller/BoatMenuController.java
+++ src/Controller/BoatMenuController.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Member;
 
 public class BoatMenuController {
     private BoatMenu menu;
-    private Member member;
+    private Model.Member member;
 
 
     public void acrUponUserInputInBoatMenu(BoatClub boatClub){
@@ -16,14 +16,18 @@ public class BoatMenuController {
         BoatMenu.UserChoiceInBoatMenu choice = menu.getUserInputInBoatMenu();
         switch (choice){
             case ADD_NEW_BOAT:
-                member= (Member) boatClub.getMember(menu.ShowAccessToMember());
-                menu.showRegisterABoat(boatClub, (Model.Member) member);
-                break;
-            case DELETE_BOAT:
-                menu.showChangeInformation(boatClub);
+                member= boatClub.getMember(menu.ShowAccessToMember());
+                menu.showRegisterABoat(boatClub, member);
+                System.out.println(member.getMemberID());
+                member.registerNewBoat(menu.getBoatType(),menu.getLength());
                 break;
             case CHANGE_BOAT_INFORMATION:
-                menu.showDeleteBoat(boatClub);
+                member= boatClub.getMember(menu.ShowAccessToMember());
+                menu.showChangeInformation(boatClub, member);
+                break;
+            case DELETE_BOAT:
+                member= boatClub.getMember(menu.ShowAccessToMember());
+                menu.showDeleteBoat(boatClub, member);
         }
     }
 }
