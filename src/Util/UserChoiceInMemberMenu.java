6687946e6e1e851522873cc5df5dfc6553142f5c
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-01 02:08:47 +0200

    Update UserChoiceInMemberMenu.java

--- src/Util/UserChoiceInMemberMenu.java
+++ src/Util/UserChoiceInMemberMenu.java
@@ -1,9 +1,7 @@
 package Util;
 
 public enum UserChoiceInMemberMenu {
-<<<<<<< HEAD
-    
-=======
+
     COMPACT_LIST, VERBOSE_LIST,QUIT,DELETE,UPDATE,SPECIFIC_MEMBER
->>>>>>> 2f82414d2e500ebbaf53c6b6d460d23cc2105a23
+
 }
