4bc4a950b06ccc281b8507e4bc81a0d7f0ee87f3
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-13 19:10:01 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         BoatClub boatClub = new BoatClub();
-        StartMenu menu=new StartMenu();
+        //StartMenu menu=new StartMenu();
 
         MainController user = new MainController(boatClub);
 
-        user.memberAction(menu);
-
+        user.memberAction();
     }
 }
