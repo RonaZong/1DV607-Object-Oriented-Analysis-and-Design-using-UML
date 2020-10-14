20a663d5a544be2bc6ba2650c9caadec114eaf03
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-14 04:03:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Controller.MainController;
 import Model.BoatClub;
 import View.StartMenu;
 
@@ -6,15 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         BoatClub boatClub = new BoatClub();
-<<<<<<< Updated upstream
-        StartMenu console = new StartMenu(boatClub);
-        console.welcomeMessage();
-=======
+
         StartMenu menu=new StartMenu();
 
         MainController user = new MainController(boatClub);
 
         user.memberAction(menu);
->>>>>>> Stashed changes
+
     }
 }
