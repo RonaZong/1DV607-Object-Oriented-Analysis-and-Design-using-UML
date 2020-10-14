1db595414e80992e5144306e887cc17f314966bc
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-14 02:00:13 +0200

    Update BoatClub.java

--- src/Model/BoatClub.java
+++ src/Model/BoatClub.java
@@ -9,6 +9,7 @@ public class BoatClub {
     public BoatClub() {
     }
 
+<<<<<<< Updated upstream
     public void creatMember(String userName, String personalNumber){
         if (this.member == null) {
             Member newMember = new Member(userName, personalNumber);
@@ -17,6 +18,41 @@ public class BoatClub {
         else {
             this.member.editMember(userName, personalNumber);
         }
+=======
+    public Member creatMember(String userName, String personalNumber) {
+        Member member = new Member(userName, personalNumber);
+        return  member;
+    }
+
+    public Member addNewMember(Member member){
+        //CompactListSave save = new CompactListSave();
+        members.add(member);
+
+       // save.saveFileOnCompactList(member);
+
+        return member;
+    }
+
+    public Member makeMemberForLoadingInStartOfProgram(String name , String personalNemuber , String memberID){
+        Member member = new Member(name , personalNemuber, memberID);
+        members.add(member);
+        return member;
+    }
+
+   /* public void saveOnVerboseList(Member member){
+      //  registry = new Registry();
+        registry.saveFile(member);
+    }*/
+
+
+    public Member deleteMember(Member member){
+        this.member = member;
+    //    registry = new Registry();
+    //   this.members = registry.loadForVerboseList(registry.verboseList("VerboseList.txt"));
+        this.members.remove(member);
+        //I used here to return that member who was deleted
+        return this.member;
+>>>>>>> Stashed changes
     }
 
     public void updateMemberInformation(Member member, String name, String personalNumber){
