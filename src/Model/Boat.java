ae5c34279485c80abee013c9afaf3d9045754e41
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-16 15:14:07 +0200

    Revert "Delete Boat.java"
    This reverts commit d4f7fa3a1ef52c0f14572e8ac31231a876dfb11a.

---
+++ src/Model/Boat.java
@@ -0,0 +1,36 @@
+package Model;
+
+
+public class Boat {
+
+    public enum BoatType {
+        SAILBOAT, MOTORSAILOR, KAYAK_OR_CANOE, OTHER
+    }
+
+    private BoatType type;
+    private double length;
+
+    //since we have only this 4 fixed type better to have enum in my idea
+    public Boat(BoatType type, double length) {
+        this.type = type;
+        this.length = length;
+    }
+
+    public BoatType getType() {
+        return type;
+    }
+
+    public void setType(BoatType type) {
+        this.type = type;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+
+}
