2b4c789822467f6be4cbe1586e0b2e260feac098
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-21 08:21:08 +0200

    Create MainController.java

---
+++ src/Controller/MainController.java
@@ -0,0 +1,58 @@
+package Controller;
+
+import Model.BoatClub;
+import Model.Member;
+import Util.MenuOptions;
+import View.MemberMenu;
+import View.ShowList;
+import View.StartMenu;
+
+public class MainController {
+
+    private BoatClub boatClub;
+    private Member member;
+    private StartMenu startMenu;
+    private MemberMenu memberMenu;
+    private ShowList showList;
+
+    public MainController(BoatClub boatClub, StartMenu startMenu) {
+        this.boatClub = boatClub;
+        this.startMenu = startMenu;
+    }
+
+    public void memberAction() {
+        while (checkInputInStartMenu()) ;
+    }
+
+    private boolean checkInputInStartMenu() {
+        this.startMenu.showInstruction();
+        int input = this.startMenu.getInputInStartMenu();
+
+        switch (input) {
+            case 1: /** MEMBER MENU */
+                checkInputInMemberMenu();
+                break;
+            case 2: /** SHOW LIST */
+                this.showList = new ShowList();
+                this.showList.showInstruction();
+                break;
+            case 3:
+                return false;
+        }
+        return true;
+    }
+
+    private void checkInputInMemberMenu() {
+        this.memberMenu = new MemberMenu();
+        this.memberMenu.showInstruction();
+
+        int input = this.memberMenu.getInputInMemberMenu();
+
+        switch (input) {
+            case 1:
+                Member member;
+        }
+    }
+
+
+}
