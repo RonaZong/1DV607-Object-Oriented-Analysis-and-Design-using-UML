ff19bac8ced9d8ec2146388e36ac5babc6f8ccd7
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:21:03 +0200

    Update Game.java

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -3,59 +3,50 @@ package Model;
 import Model.rules.RulesFactory;
 
 public class Game {
-    private Dealer m_dealer;
-    private Player m_player;
+    private Dealer dealer;
+    private Player player;
 
-    public Game()
-    {
-        m_dealer = new Dealer(new RulesFactory());
-        m_player = new Player();
+    public Game() {
+        this.dealer = new Dealer(new RulesFactory());
+        this.player = new Player();
     }
 
-
-    public boolean IsGameOver()
-    {
-        return m_dealer.IsGameOver();
+    public boolean IsGameOver() {
+        return this.dealer.IsGameOver();
     }
 
-    public boolean IsDealerWinner()
-    {
-        return m_dealer.IsDealerWinner(m_player);
+    public boolean IsDealerWinner() {
+        return this.dealer.IsDealerWinner(player);
     }
 
-    public boolean NewGame()
-    {
-        return m_dealer.NewGame(m_player);
+    public boolean NewGame() {
+        return this.dealer.NewGame(player);
     }
 
-    public boolean Hit()
-    {
-        return m_dealer.Hit(m_player);
+    /** take a card */
+    public boolean Hit() {
+        return this.dealer.Hit(player);
     }
 
-    public boolean Stand()
-    {
+    /** end turn */
+    public boolean Stand() {
         // TODO: Implement this according to Game_Stand.sequencediagram
-        return true;
+        return this.dealer.Stand();
     }
 
-    public Iterable<Card> GetDealerHand()
-    {
-        return m_dealer.GetHand();
+    public Iterable<Card> GetDealerHand() {
+        return this.dealer.GetHand();
     }
 
-    public Iterable<Card> GetPlayerHand()
-    {
-        return m_player.GetHand();
+    public Iterable<Card> GetPlayerHand() {
+        return this.player.GetHand();
     }
 
-    public int GetDealerScore()
-    {
-        return m_dealer.CalcScore();
+    public int GetDealerScore() {
+        return this.dealer.CalcScore();
     }
 
-    public int GetPlayerScore()
-    {
-        return m_player.CalcScore();
+    public int GetPlayerScore() {
+        return this.player.CalcScore();
     }
 }
