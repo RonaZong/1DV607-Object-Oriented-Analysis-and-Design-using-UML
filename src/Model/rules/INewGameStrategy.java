52540ea6062b2f3d5413de388935e49f6eb067ba
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:20:47 +0200

    Delete INewGameStrategy.java

--- src/Model/rules/INewGameStrategy.java
+++
@@ -1,9 +0,0 @@
-package Model.rules;
-
-import Model.Dealer;
-import Model.Deck;
-import Model.Player;
-
-public interface INewGameStrategy {
-    boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player);
-}
