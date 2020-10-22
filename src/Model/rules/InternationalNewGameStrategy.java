7d6f13e69bc95a906efd77979697369031f7e95f
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:20:45 +0200

    Update InternationalNewGameStrategy.java

--- src/Model/rules/InternationalNewGameStrategy.java
+++ src/Model/rules/InternationalNewGameStrategy.java
@@ -5,19 +5,19 @@ import Model.Dealer;
 import Model.Deck;
 import Model.Player;
 
-class InternationalNewGameStrategy implements INewGameStrategy{
-    public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
+class InternationalNewGameStrategy implements NewGameStrategy {
+    public boolean NewGame(Deck deck, Dealer dealer, Player a_player) {
         Card c;
 
-        c = a_deck.GetCard();
+        c = deck.GetCard();
         c.Show(true);
         a_player.DealCard(c);
 
-        c = a_deck.GetCard();
+        c = deck.GetCard();
         c.Show(true);
-        a_dealer.DealCard(c);
+        dealer.DealCard(c);
 
-        c = a_deck.GetCard();
+        c = deck.GetCard();
         c.Show(true);
         a_player.DealCard(c);
 
