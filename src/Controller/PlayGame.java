0e30e9ce4f27dc4893f82d9588ac3d22cd5c2b90
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:21:11 +0200

    Update PlayGame.java

--- src/Controller/PlayGame.java
+++ src/Controller/PlayGame.java
@@ -4,32 +4,26 @@ import Model.Game;
 import View.IView;
 
 public class PlayGame {
-    public boolean Play(Game a_game, IView a_view) {
-        a_view.DisplayWelcomeMessage();
+    public boolean Play(Game game, IView view) {
+        view.DisplayWelcomeMessage();
 
-        a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
-        a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+        view.DisplayDealerHand(game.GetDealerHand(), game.GetDealerScore());
+        view.DisplayPlayerHand(game.GetPlayerHand(), game.GetPlayerScore());
 
-        if (a_game.IsGameOver())
-        {
-            a_view.DisplayGameOver(a_game.IsDealerWinner());
+        if (game.IsGameOver()) {
+            view.DisplayGameOver(game.IsDealerWinner());
         }
 
-        int input = a_view.GetInput();
-
-        if (input == 'p')
-        {
-            a_game.NewGame();
+        if (view.play()) {
+            game.NewGame();
         }
-        else if (input == 'h')
-        {
-            a_game.Hit();
+        else if (view.hit()) {
+            game.Hit();
         }
-        else if (input == 's')
-        {
-            a_game.Stand();
+        else if (view.stand()) {
+            game.Stand();
         }
 
-        return input != 'q';
+        return view.quit();
     }
 }
