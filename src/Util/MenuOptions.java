b7acada97eb6897581848a0a774bf46f98ab7114
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 10:48:54 +0200

    Update MenuOptions.java

--- src/Util/MenuOptions.java
+++ src/Util/MenuOptions.java
@@ -2,20 +2,33 @@ package Util;
 
 public enum MenuOptions {
 
+    /** Start Menu */
     QUIT(0, "QUIT"),
     MEMBER_MENU(1, "MEMBER MENU"),
     SHOW_LIST(2, "SHOW LIST"),
 
+    /** Member Menu */
     ADD_MEMBER(1, "ADD MEMBER"),
     UPDATE_MEMBER(2,"UPDATE MEMBER"),
     DELETE_MEMBER(3, "DELETE MEMBER"),
 
+    /** Show List */
+    COMPACT_LIST(1, "COMPACT LIST"),
+    VERBOSE_LIST(2, "VERBOSE LIST"),
+
+    /** Boat Menu */
     ADD_BOAT(1, "ADD Boat"),
     UPDATE_BOAT(2, "UPDATE BOAT"),
     DELETE_BOAT(3, "DELETE BOAT"),
 
-    COMPACT_LIST(1, "COMPACT LIST"),
-    VERBOSE_LIST(2, "VERBOSE LIST");
+    /** Boat Type */
+    SAILBOAT(1, "SAILBOAT"),
+    MOTORSAILOR(2, "MOTORSAILOR"),
+    KAYAK_OR_CANOE(3, "KAYAK_OR_CANOE"),
+    OTHER(4, "OTHER");
+
+
+
 
     private int input;
     private String message;
