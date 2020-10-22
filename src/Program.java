5d7b16e0b316419abe1d9835680d664094737609
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 12:43:59 +0200

    Update Program.java

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+import Controller.PlayGame;
+import Model.Game;
+import View.IView;
+import View.SimpleView;
+
+public class Program {
+
+    public static void main(String[] a_args)
+    {
+
+        Game g = new Game();
+        IView v = new SimpleView(); //new SwedishView();
+        PlayGame ctrl = new PlayGame();
+
+        while (ctrl.Play(g, v));
+    }
+}
