ee3cb6c723b5bfe02f8e6dfc5e513cd81724033f
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:20:38 +0200

    Create BaseView.java

---
+++ src/View/BaseView.java
@@ -0,0 +1,20 @@
+package View;
+
+import Model.Card;
+
+public abstract class BaseView {
+    protected int getInput()
+    {
+        try {
+            int c = System.in.read();
+            while (c == '\r' || c =='\n') {
+                c = System.in.read();
+            }
+            return c;
+        } catch (java.io.IOException e) {
+            System.out.println("" + e);
+            return 0;
+        }
+    }
+
+}
