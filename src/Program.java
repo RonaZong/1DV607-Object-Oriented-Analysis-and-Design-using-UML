b9388d3319d18fee57acd17d3dcb7f59d13c9d41
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 19:05:59 +0200

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -7,7 +7,6 @@ public class Program {
 
     public static void main(String[] a_args)
     {
-
         Game g = new Game();
         IView v = new SimpleView(); //new SwedishView();
         PlayGame ctrl = new PlayGame();
