1a05d1e3cedf5548cec97015ae489fa8761c9b26
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:21:38 +0100

    Update PlayGame.java

--- src/Controller/PlayGame.java
+++ src/Controller/PlayGame.java
@@ -1,29 +1,57 @@
 package Controller;
 
-import Model.Game;
+import Model.*;
 import View.IView;
 
-public class PlayGame {
-    public boolean Play(Game game, IView view) {
-        view.DisplayWelcomeMessage();
+public class PlayGame implements CardObserver {
+    private Game game;
+    private IView view;
 
-        view.DisplayDealerHand(game.GetDealerHand(), game.GetDealerScore());
-        view.DisplayPlayerHand(game.GetPlayerHand(), game.GetPlayerScore());
+    public PlayGame(Game game, IView view) {
+        this.view = view;
+        this.game = game;
+    }
 
-        if (game.IsGameOver()) {
-            view.DisplayGameOver(game.IsDealerWinner());
+    @Override
+    public void updateNewCard(Card card) {
+        this.view.DisplayPlayerCard(card);
+        this.view.Pause();
+        this.view.DisplayDealerCard(card);
+
+    }
+
+    @Override
+    public void GotCard(String name, Card.Value cardValue, Card.Color cardColor) {
+        System.out.println(name + ": " + cardValue + " of " + cardColor);
+    }
+
+    public boolean Play() {
+        this.view.DisplayWelcomeMessage();
+
+        this.view.DisplayDealerHand(this.game.GetDealerHand(), this.game.GetDealerScore());
+        this.view.DisplayPlayerHand(this.game.GetPlayerHand(), this.game.GetPlayerScore());
+
+        if (this.game.IsGameOver()) {
+            this.view.DisplayGameOver(this.game.IsDealerWinner());
         }
 
-        if (view.play()) {
-            game.NewGame();
-        }
-        else if (view.hit()) {
-            game.Hit();
-        }
-        else if (view.stand()) {
-            game.Stand();
+        this.view.collectEvents();
+
+        if (this.view.play()) {
+            this.game.NewGame();
+        } else if (this.view.hit()) {
+            this.game.Hit();
+        } else if (this.view.stand()) {
+            this.game.Stand();
         }
 
-        return view.quit();
+//        for (int i = 0; i <= ((Collection<?>) this.game.GetPlayerHand()).size(); i++) {
+//            for (int k = 0; k <= ((Collection<?>) this.game.GetDealerHand()).size(); k++) {
+//                this.game.GetPlayerCard(i);
+//                this.game.GetDealerCard(k);
+//            }
+//        }
+
+        return !this.view.quit();
     }
 }
