003d91c34bd3f83295cfedc4f6130c974985bc8e
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:21:11 +0100

    Update BasicHitStrategy.java

--- src/Model/rules/BasicHitStrategy.java
+++ src/Model/rules/BasicHitStrategy.java
@@ -4,11 +4,18 @@ import Model.Player;
 
 public class BasicHitStrategy implements HitStrategy {
     /** Soft 17 */
-    private final int g_hitLimit = 17;
+    private final int hitLimit = 17;
 
     // we have to add new rules
     // when dealer should take one more card
     public boolean DoHit(Player dealer) {
-        return dealer.CalcScore() < g_hitLimit;
+        return dealer.CalcScore() < hitLimit;
+    }
+
+    public boolean Soft17(Player dealer) {
+        if (dealer.Soft17()) {
+            return dealer.CalcScore() == hitLimit;
+        }
+        return false;
     }
 }
