418f50316315afd6a5e645d7c125e0f0f3d4dc19
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:20:52 +0100

    Update IView.java

--- src/View/IView.java
+++ src/View/IView.java
@@ -1,17 +1,19 @@
 package View;
 
 import Model.Card;
-import Model.Dealer;
-
-import java.util.Scanner;
 
 public interface IView {
     boolean play();
     boolean hit();
     boolean stand();
     boolean quit();
+    void collectEvents();
+    void Pause();
+
     void DisplayWelcomeMessage();
-    void DisplayCard(Card card);
+    void DisplayPlayerCard(Card card);
+    void DisplayDealerCard(Card card);
+    void DisplayCard(String name, Card card);
     void DisplayPlayerHand(Iterable<Card> hand, int score);
     void DisplayDealerHand(Iterable<Card> hand, int score);
     void DisplayGameOver(boolean dealerIsWinner);
