3b3c97e41a6840373925d1e97c37c4e590a81b2c
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:21:13 +0100

    Update AmericanNewGameStrategy.java

--- src/Model/rules/AmericanNewGameStrategy.java
+++ src/Model/rules/AmericanNewGameStrategy.java
@@ -7,29 +7,10 @@ import Model.Player;
 
 class AmericanNewGameStrategy extends Factory implements NewGameStrategy {
     public boolean NewGame(Deck deck, Dealer dealer, Player player) {
-//        Card c;
-
-        /** Rona added */
         getCard(deck, player, true);
         getCard(deck, dealer, true);
         getCard(deck, player, true);
         getCard(deck, dealer, false);
-
-//        c = deck.GetCard();
-//        c.Show(true);
-//        player.DealCard(c);
-//
-//        c = deck.GetCard();
-//        c.Show(true);
-//        dealer.DealCard(c);
-//
-//        c = deck.GetCard();
-//        c.Show(true);
-//        player.DealCard(c);
-//
-//        c = deck.GetCard();
-//        c.Show(false);
-//        dealer.DealCard(c);
 
         return true;
     }
