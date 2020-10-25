921cad84fa67671bb045af674265863408d4ff6e
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:21:05 +0100

    Update InternationalNewGameStrategy.java

--- src/Model/rules/InternationalNewGameStrategy.java
+++ src/Model/rules/InternationalNewGameStrategy.java
@@ -1,30 +1,14 @@
 package Model.rules;
 
-import Model.Card;
 import Model.Dealer;
 import Model.Deck;
 import Model.Player;
 
 class InternationalNewGameStrategy extends Factory implements NewGameStrategy {
     public boolean NewGame(Deck deck, Dealer dealer, Player player) {
-        /** Rona added */
         getCard(deck, player, true);
         getCard(deck, dealer, true);
         getCard(deck, player, true);
-
-//        Card c;
-//
-//        c = deck.GetCard();
-//        c.Show(true);
-//        a_player.DealCard(c);
-//
-//        c = deck.GetCard();
-//        c.Show(true);
-//        dealer.DealCard(c);
-//
-//        c = deck.GetCard();
-//        c.Show(true);
-//        a_player.DealCard(c);
 
         return true;
     }
