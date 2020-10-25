bd0dd5aa2a4b13ecd20567063e89938365ad3063
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:21:25 +0100

    Update Deck.java

--- src/Model/Deck.java
+++ src/Model/Deck.java
@@ -3,45 +3,64 @@ package Model;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Deck {
+public class Deck implements CardObserver{
     private List<Card> cards;
+    private Card card;
 
-    public Deck() {
-        cards = new LinkedList<Card>();
+    private static int observerIDTracker = 0;
+    private int observerID;
 
-        for(int cIx = 0; cIx < Card.Color.Count.ordinal(); cIx++) {
-            for (int vIx = 0; vIx < Card.Value.Count.ordinal(); vIx++) {
-                Card c = new Card(Card.Color.values()[cIx], Card.Value.values()[vIx]);
-                AddCard(c);
+
+    public Deck(Card card) {
+        this.cards = new LinkedList<Card>();
+        this.card = card;
+
+        for(int cIx = 0; cIx < Card.Value.Count.ordinal(); cIx++) {
+            for (int vIx = 0; vIx < Card.Color.Count.ordinal(); vIx++) {
+                this.card = new Card(Card.Value.values()[cIx], Card.Color.values()[vIx]);
+                AddCard(this.card);
+                this.observerID = ++observerIDTracker;
+                System.out.println("New Observer " + this.observerID);
+                card.register(this);
             }
         }
 
         Shuffle();
     }
 
+    public Iterable<Card> GetCards() {
+        return this.cards;
+    }
 
     public void AddCard(Card cardToAdd) {
-        cards.add(cardToAdd);
+        this.cards.add(cardToAdd);
     }
 
     public Card GetCard() {
-        Card c = cards.get(0);
-        cards.remove(0);
+        Card card = this.cards.get(0);
+        this.cards.remove(0);
 
-        return c;
+        return card;
     }
 
-    public Iterable<Card> GetCards() {
-        return cards;
-    }
-
-    private void Shuffle()
-    {
+    private void Shuffle() {
         for (int i = 0; i < 1017; i++) {
-            int index = (int)(Math.random() * 171717.0) % cards.size();
-            Card c = cards.get(index);
-            cards.remove(index);
+            int index = (int)(Math.random() * 171717.0) % this.cards.size();
+            Card c = this.cards.get(index);
+            this.cards.remove(index);
             AddCard(c);
         }
+    }
+
+    @Override
+    public void update(Card.Value cardValue, Card.Color cardColor) {
+        this.card.GetValue() = cardValue;
+        this.card.GetColor() = cardColor;
+
+        printTheCard();
+    }
+
+    public void printTheCard() {
+        System.out.println(observerID + "\n" + );
     }
 }
