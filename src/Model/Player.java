9987e2c9b0746d66533ba884cdd6c5ea472c7a5b
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 13:21:15 +0100

    Update Player.java

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,9 +1,10 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Player {
+public class Player  {
     private List<Card> hand;
     protected final int maxScore = 21;
 
@@ -44,9 +45,9 @@ public class Player {
             }
         }
 
-        if (score > maxScore) {
+        if (score > this.maxScore) {
             for(Card c : GetHand()) {
-                if (c.GetValue() == Card.Value.Ace && score > maxScore) {
+                if (c.GetValue() == Card.Value.Ace && score > this.maxScore) {
                     score -= 10;
                 }
             }
@@ -54,4 +55,26 @@ public class Player {
 
         return score;
     }
+
+    public boolean Soft17() {
+        int cardScores[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10 ,10 ,10, 11 };
+        assert (cardScores.length == Card.Value.Count.ordinal()) : "Card Scores array size does not match number of card values";
+
+        int score = 0;
+
+        for (Card card : GetHand()) {
+            if (card.GetValue() == Card.Value.Ace) {
+                score += cardScores[card.GetValue().ordinal()];
+
+                if (score > this.maxScore) {
+                    score -= 10;
+                }
+                return score == 17;
+            }
+        }
+
+        return false;
+    }
+
+
 }
