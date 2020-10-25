d8d4b1117f14e6fe65a9f84f371f859e096b21a1
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-25 17:33:24 +0100

    Update WinnerStrategy.java

--- src/Model/rules/WinnerStrategy.java
+++ src/Model/rules/WinnerStrategy.java
@@ -4,24 +4,22 @@ package Model.rules;
 import Model.Dealer;
 import Model.Player;
 
-public class WinnerStrategy implements IWinnerStrategy{
-    public boolean IsDealerWinner(Player a_player, Dealer a_dealer, final int g_maxScore) {
-        System.out.println("a");
-//        if (a_player.CalcScore() > g_maxScore) {
-//            return true;
-//        } else if (a_dealer.CalcScore() > g_maxScore) {
-//            return false;
-//        }
-        return a_dealer.CalcScore() == a_player.CalcScore(); // dealer win
+public class WinnerStrategy implements WinStrategy {
+    public boolean IsPlayerWinner(Player player, Dealer dealer, final int maxScore) {
+        if (player.CalcScore() > maxScore) {
+            return true;
+        } else if (dealer.CalcScore() > maxScore) {
+            return false;
+        }
+        return player.CalcScore() >= dealer.CalcScore();
     }
 
-    public boolean GeneralWinner(Player a_player, Dealer a_dealer, final int g_maxScore) {
-        System.out.println("a");
-        if (a_player.CalcScore() > g_maxScore) {
+    public boolean IsDealerWinner(Player player, Dealer dealer, final int maxScore) {
+        if (player.CalcScore() > maxScore) {
             return true; // player lose
-        } else if (a_dealer.CalcScore() > g_maxScore) {
+        } else if (dealer.CalcScore() > maxScore) {
             return false; // player win
         }
-        return a_dealer.CalcScore() > a_player.CalcScore(); // player win
+        return dealer.CalcScore() >= player.CalcScore(); // player win
     }
 }
