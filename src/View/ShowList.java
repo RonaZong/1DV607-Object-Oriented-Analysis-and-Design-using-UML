0d21af4a4d1a1d03e0b7ee73a7794cc3651a7bd9
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-26 10:32:01 +0100

    Update ShowList.java

--- src/View/ShowList.java
+++ src/View/ShowList.java
@@ -1,15 +1,21 @@
 package View;
 
+import Model.Boat;
+import Model.BoatClub;
+import Model.Member;
+import Util.MenuOptions;
+
 import java.util.Scanner;
 
-public class ShowList implements IView{
+public class ShowList implements IView {
     private Scanner sc = new Scanner(System.in);
+    private MenuOptions menuOptions;
 
     public ShowList() {
     }
 
     @Override
-    public void showInstruction(){
+    public void showInstruction() {
         System.out.println("----Show List----\n" +
                 "Press 1 to show COMPACT LIST\n" +
                 "Press 2 to show VERBOSE LIST\n" +
@@ -20,6 +26,54 @@ public class ShowList implements IView{
 
     @Override
     public int getInput() {
-        return 0;
+        int inputOfShowListMenu = sc.nextInt();
+        switch (inputOfShowListMenu) {
+            case 1:
+                menuOptions = MenuOptions.COMPACT_LIST;
+                if (menuOptions.getInput() == inputOfShowListMenu) {
+                    System.out.println(menuOptions.getMessage());
+                }
+                break;
+            case 2:
+                menuOptions = MenuOptions.VERBOSE_LIST;
+                if (menuOptions.getInput() == inputOfShowListMenu) {
+                    System.out.println(menuOptions.getMessage());
+                }
+                break;
+            case 3:
+                menuOptions = MenuOptions.SPECIFIC_MEMBER;
+                if (menuOptions.getInput() == inputOfShowListMenu) {
+                    System.out.println(menuOptions.getMessage());
+                }
+        }
+        return inputOfShowListMenu;
+    }
+
+
+    public void showCompactList(BoatClub boatClub) {
+        for (Member member : boatClub.getAllMember()) {
+            System.out.println("This member name is : " + member.getName() +
+                    "\nwith memberID of : " + member.getMemberID() +
+                    "\nwhich has " + member.boatsOwnedByMember().size() + "boats" +
+                    "\n------------\n");
+        }
+    }
+
+    public void showVerboseList(BoatClub boatClub) {
+        for (Member member : boatClub.getAllMember()) {
+            System.out.println("This member name is : " + member.getName() +
+                    "\nwith personal number of " + member.getPersonalNumber() +
+                    "\nwith memberID of " + member.getMemberID());
+            //it might give a null exception
+            System.out.println("This member has " + member.boatsOwnedByMember().size() + "boats");
+            if (member.boatsOwnedByMember().size() > 0) {
+                System.out.println("this member boat information is :");
+                for (Boat boat : member.boatsOwnedByMember()) {
+                    System.out.println("Boat type :" + boat.getType() +
+                            "\nBoat color : " + boat.getLength());
+                }
+            }
+        }
     }
 }
+
