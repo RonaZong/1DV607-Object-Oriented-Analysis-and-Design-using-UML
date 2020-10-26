8e929e59cf7e59e4d4874e7946fe85e0ffd36ff9
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-26 15:38:13 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import Controller.MainController;
 import Model.BoatClub;
+import Model.Registry;
 import View.StartMenu;
 
 public class Main {
 
     public static void main(String[] args) {
+        Registry registry = new Registry("Registry.txt");
         BoatClub boatClub = new BoatClub();
         StartMenu startMenu = new StartMenu();
         MainController mainController = new MainController(boatClub, startMenu);
 
         mainController.memberAction();
+        registry.saveTextToFile(mainController.save());
     }
 }
