a8b372ed521919f3fe0d09df2b8badce3ec28d8a
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-26 16:08:06 +0100

    Update BoatClub.java

--- src/Model/BoatClub.java
+++ src/Model/BoatClub.java
@@ -2,35 +2,23 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BoatClub {
-    private Map<Integer, Member> membersMap = new HashMap<>();
-    private ArrayList<Member> members;
+    private Map<Integer, Member> membersMap;
     private Member member;
     private int memberID ;
 
     public BoatClub() {
-        this.members = new ArrayList<>();
+        this.membersMap = new HashMap<>();
         this.memberID = 0;
     }
 
-    public String memberToString(int memberID) {
-        if( (membersMap.size() > 0) && (memberID <= membersMap.size()) )
-            return membersMap.get(memberID).toStringExtended(false);
-        else
-            return "\nNo member found. Please create new member.";
-    }
 
-    public void addMember() {
-        this.members.add(this.member);
-    }
-
-    public void creatMember(String personalNumber, String userName, String password){
-        memberID++;
-        Member newMember = new Member(personalNumber, userName,password);
+    public void addMember(Member newMember){
+        this.memberID++;
         this.member = newMember;
-        addMember();
         this.membersMap.put(this.memberID, this.member);
     }
 
@@ -42,13 +30,74 @@ public class BoatClub {
     }
 
     public void deleteMember(Member member){
-        this.members.remove(member);
+//        this.members.remove(member);
         this.membersMap.remove(member.getMemberID());
     }
 
-    public ArrayList<Member> getAllMember(){
-        return this.members;
+//    public ArrayList<Member> getAllMember(){
+//        return this.members;
+//    }
+
+    public String memberToString(int memberID) {
+        if( (membersMap.size() > 0) && (memberID <= membersMap.size()) )
+            return membersMap.get(memberID).toString();
+        else
+            return "\nNo member found. Please create new member.";
     }
 
+    public List<String> save() {
+        List<String> memberRegistry = new ArrayList<>();
 
+        // Adding header information.
+        memberRegistry.add(Integer.toString(this.member.getMemberID()));
+
+        for (Map.Entry<Integer, Member> entry : membersMap.entrySet()) {
+            memberRegistry.add( entry.getValue().getMemberID()+", "+
+                    entry.getValue().getName()+", "+
+                    entry.getValue().getPersonalNumber()+", " +
+                    entry.);
+        }
+        return memberRegistry;
+    }
+
+    public String membersToString(boolean compact) {
+        String membersString = "";
+        if (this.membersMap.size() > 0) {
+            for (Map.Entry<Integer, Member> entry : this.membersMap.entrySet()) {
+//                membersString += "\n" + (entry.getValue()).toString(compact);
+            }
+        } else {
+            membersString = "\n No members found.";
+        }
+        return membersString;
+    }
+
+    public void polulate(List<String> memberRegistry) {
+        // If nothing loaded ignore populating.
+        if (memberRegistry == null) {
+            return;
+        }
+
+        // Updating memberID counter.
+        this.memberID = Integer.parseInt(memberRegistry.remove(0));
+
+        String[] parsedLine; // The parsed CSV temporary storage.
+
+        // Parse CSV string.
+        for (String textLine : memberRegistry) {
+            parsedLine = textLine.split(","); // Parse CSV.
+
+            // Parse the header for a member, name, id, personal number.
+            this.membersMap.put(Integer.parseInt(parsedLine[0]), new Member(parsedLine[1], parsedLine[2], Integer.parseInt(parsedLine[0])));
+
+            // Check if there are any boats associated to the member.
+            for (int i = 3; i < parsedLine.length - 1; i += 2)
+                this.member.addBoat(parsedLine[i], Integer.parseInt(parsedLine[i + 1]), Integer.parseInt(parsedLine[0]));
+        }
+
+    }
+
+    public boolean memberExists(int memberID) {
+        return this.membersMap.containsKey(memberID);
+    }
 }
