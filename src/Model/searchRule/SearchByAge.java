9445df2d7667fd000626ffcad03afe3dfdc8c1c5
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-27 16:20:40 +0100

    Update SearchByAge.java

--- src/Model/searchRule/SearchByAge.java
+++ src/Model/searchRule/SearchByAge.java
@@ -3,17 +3,19 @@ package Model.searchRule;
 import Model.BoatClub;
 import Model.Member;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class SearchByAge implements ISearchingStrategy {
+
+    private int year;
+
+    public SearchByAge(int input){
+       this.year = input;
+    }
+
     @Override
-    public Iterable<Member> searchMembers(BoatClub boatClub,String input) {
-        ArrayList<Member> members = new ArrayList<>();
-        for(Member member : boatClub.getAllMembersLocally()){
-             if(Integer.parseInt(member.getPersonalNumber().toString().substring(0,4))<Integer.parseInt(input))
-                 members.add(member);
-             return members;
-        }
-        return null;
+    public boolean searchMembers(Member member) {
+        return (Integer.parseInt(member.getPersonalNumber().toString().substring(0,4))< year);
     }
 }
