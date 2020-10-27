a8a169715f0afc93ee1e903ca3053b945e74beee
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-27 16:19:28 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
 import Controller.MainController;
 import Model.BoatClub;
+import Model.Member;
+import Model.PersonalNumber;
+import Model.searchRule.CompositeSearch;
 import Model.searchRule.ISearchingStrategy;
 import Model.searchRule.SearchByAge;
+import Model.searchRule.SearchNameStartWithString;
+import Util.Checksum;
 import View.StartMenu;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ISearchingStrategy search = new SearchByAge();
-        BoatClub boatClub = new BoatClub(search);
-        StartMenu menu=new StartMenu();
+        //System.out.print(PersonalNumber.DigitsForChecksum);
 
+        BoatClub boatClub = new BoatClub();
+
+        StartMenu menu=new StartMenu();
+        System.out.print(Checksum.R1.getDigit());
         MainController user = new MainController(boatClub);
 
         user.memberAction(menu);
+
+        ISearchingStrategy search = new SearchByAge(1990);
+        ISearchingStrategy search1 = new SearchNameStartWithString("ni");
+        CompositeSearch cm = new CompositeSearch();
+        cm.add(search);
+        cm.add(search1);
+        Iterable<Member> members = boatClub.searchForMember(cm);
+        for(Member m : members)
+            System.out.println(m.getName() + " , " + m.getPersonalNumber());
 
     }
 }
