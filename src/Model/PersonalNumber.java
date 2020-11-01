918216057e6da050c3f5cc2a8579a6ac7b8b2253
Rahim Pormah <rp222hu@student.lnu.se>
2020-11-01 23:09:32 +0100

    Update PersonalNumber.java

--- src/Model/PersonalNumber.java
+++ src/Model/PersonalNumber.java
@@ -1,19 +1,10 @@
 package Model;
 
-import javax.swing.text.DateFormatter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.InputMismatchException;
 
 
 public class PersonalNumber {
-    public enum DigitsForChecksum{
-      DIGITS(new int[]{1,2,3,4,5,6,7,8,9,0});
-      private int[] input;
-      private DigitsForChecksum(int[] newInput){
-          input = newInput;
-      }
-    }
 
     private LocalDate date;
     // private DateFormat birthday = new DateFormat();
@@ -21,12 +12,14 @@ public class PersonalNumber {
     private boolean valid;
     private String birthDate;
     private String personalNumber;
+    private Last3DigitsBeforeChecksum last3DigitsBeforeChecksum;
     private Checksum checksum;
 
         public PersonalNumber(){
         }
-        public PersonalNumber(LocalDate newDate , Checksum checksum){
+        public PersonalNumber(LocalDate newDate , Last3DigitsBeforeChecksum last3DigitsBeforeChecksum,Checksum checksum){
            setDate(newDate);
+           setLast3DigitsBeforeChecksum(last3DigitsBeforeChecksum);
            setChecksum(checksum);
             //date =LocalDate.parse(id.substring(0,8), DateTimeFormatter.BASIC_ISO_DATE);
            // setChecksum(id.substring(9));
@@ -34,6 +27,9 @@ public class PersonalNumber {
            // this.id = id;
         }
 
+    private void setLast3DigitsBeforeChecksum(Last3DigitsBeforeChecksum last3DigitsBeforeChecksum) {
+            this.last3DigitsBeforeChecksum = last3DigitsBeforeChecksum;
+    }
 
 
     public PersonalNumber(String dataFromRegistry){
@@ -51,27 +47,10 @@ public class PersonalNumber {
         this.personalNumber = personalNumber;
     }
 
-   /* public String toString(){
-            return this.personalNumber;
-    }*/
+    public LocalDate getDate(){
+            return this.date;
+    }
 
-   /* public String showID(){
-            return date.toString() + checksum;
-        }
-        public int getChecksum(){
-            return checksum;
-        }*/
-
-//        public int comparedTo(PersonalNumber otherID){
-//            if (showID().substring(0,7).compareTo(otherID.showID().substring(0,7)) == 0)
-//                return 0;
-//            else if (showID().substring(0,7).compareTo(otherID.showID().substring(0,7)) > 0){
-//                return 1;
-//            }
-//            else  {
-//                return -1;
-//            }
-//        }
         public boolean validChecksum(){
             String birth = date.format(DateTimeFormatter.BASIC_ISO_DATE);
             int ch2 = Integer.parseInt(birth.substring(2,3)) * 2;
@@ -80,13 +59,13 @@ public class PersonalNumber {
             int ch5 = Integer.parseInt(birth.substring(5,6));
             int ch6 = Integer.parseInt(birth.substring(6,7)) * 2;
             int ch7 = Integer.parseInt(birth.substring(7,8));
-            int ch9 = Integer.parseInt(checksum.getChecksum().substring(0,1)) * 2;
-            int ch10 = Integer.parseInt(checksum.getChecksum().substring(1,2));
-            int ch11 = Integer.parseInt(checksum.getChecksum().substring(2,3)) * 2;
+            int ch9 = Integer.parseInt(last3DigitsBeforeChecksum.toString().substring(0,1)) * 2;
+            int ch10 = Integer.parseInt(last3DigitsBeforeChecksum.toString().substring(1,2));
+            int ch11 = Integer.parseInt(last3DigitsBeforeChecksum.toString().substring(2,3))* 2;
             int sum = ch2 / 10 + ch2 % 10 + ch3 / 10 + ch3 % 10 + ch4 / 10 + ch4 % 10 + ch5 / 10 + ch5 % 10 + ch6 / 10 + ch6 % 10
                     + ch7 / 10 + ch7 % 10 + ch9 / 10 + ch9 % 10 + ch10 / 10 + ch10 % 10 + ch11 / 10 + ch11 % 10;
             int chech = (10 - sum % 10) % 10;
-            int checksum1 = Integer.parseInt(checksum.getChecksum().substring(3));
+            int checksum1 = this.checksum.getNumber();
             if (checksum1 % 10 == chech){
                 return true;
             }
@@ -108,11 +87,25 @@ public class PersonalNumber {
         return date.format(DateTimeFormatter.BASIC_ISO_DATE);
     }
 
-//    public String getString(){
-//            return personalNumber;
-//    }
+    public Last3DigitsBeforeChecksum getLast3DigitsBeforeChecksum(){
+            return this.last3DigitsBeforeChecksum;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+            if(obj instanceof PersonalNumber){
+                PersonalNumber newPersonalNumber = (PersonalNumber)obj;
+                return this.date.compareTo(newPersonalNumber.getDate())==0 && this.last3DigitsBeforeChecksum.equals(newPersonalNumber.getLast3DigitsBeforeChecksum())
+                        &&this.checksum.equals(newPersonalNumber.getChecksum());
+            }
+            return false;
+    }
+
+    public Checksum getChecksum(){
+            return this.checksum;
+    }
 
     public String getPersonalNumber(){
-            return date.format(DateTimeFormatter.BASIC_ISO_DATE) + checksum.getChecksum();
+            return date.format(DateTimeFormatter.BASIC_ISO_DATE) + this.last3DigitsBeforeChecksum.toString() +this.checksum.toString();
     }
 }
